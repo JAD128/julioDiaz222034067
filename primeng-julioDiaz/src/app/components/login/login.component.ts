a6abf7c5ff5bc93c784e1077ecaf0b3c80aa6d1d
JAD128 <juandima12@gmail.com>
2024-10-19 11:52:52 -0500

    feat : new changes

--- primeng-julioDiaz/src/app/components/login/login.component.ts
+++ primeng-julioDiaz/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CardModule } from 'primeng/card';
+import { Password } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,8 @@ export class LoginComponent {
 
   loginForm = this.fb.group(
     {
-      usuario : ['', [Validators.required, Validators.required]]
+      usuario : ['', [Validators.required, Validators.name]],
+      password : ['', Validators.required, Validators]
     }
   );
   constructor(private fb : FormBuilder){

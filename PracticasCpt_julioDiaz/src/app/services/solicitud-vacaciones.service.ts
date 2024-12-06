10a2105b24289862d3f2eb6daf244de8bc42e624
JAD128 <juandima12@gmail.com>
2024-12-06 17:38:05 -0500

    feat : new files

---
+++ PracticasCpt_julioDiaz/src/app/services/solicitud-vacaciones.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SolicitudVacacionesService {
+
+  apiUrl = 'https://localhost:7233/api/UsersVacations'
+
+  constructor(private http : HttpClient) { }
+
+  register(name : string,lastName : string, email :string, phone : string, name_g : string, email_g : string, date_ini : string, date_fi : string, notes : string):Observable<any>{
+    return this.http.post(`${this.apiUrl}/register`, {name,lastName,email,phone,name_g,email_g,date_ini,date_fi,notes})
+  }
+}
--- PracticasCpt_julioDiaz/src/app/vacationsform/vacationsform.component.ts
+++ PracticasCpt_julioDiaz/src/app/vacationsform/vacationsform.component.ts
@@ -5,18 +5,20 @@ import { LicenseComponent } from '../components/license/license.component';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { SolicitudVacacionesService } from '../services/solicitud-vacaciones.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-vacationsform',
   standalone: true,
-  imports: [GInformationComponent, PInformationComponent, LicenseComponent, CommonModule, ReactiveFormsModule, ButtonModule],
+  imports: [GInformationComponent, PInformationComponent, LicenseComponent, CommonModule, ReactiveFormsModule, ButtonModule, HttpClientModule],
   templateUrl: './vacationsform.component.html',
   styleUrl: './vacationsform.component.css'
 })
 export class VacationsformComponent {
   vacationsForm : FormGroup;
 
-  constructor(private fb : FormBuilder){
+  constructor(private fb : FormBuilder, private registerService : SolicitudVacacionesService){
     this.vacationsForm = this.fb.group(
       {
         name_g : ['', Validators.required],
@@ -33,11 +35,24 @@ export class VacationsformComponent {
   }
 
   onSubmit(){
-    if(this.vacationsForm.valid){
-      console.log(this.vacationsForm.value)
+    if (this.vacationsForm.valid){
+      console.log(this.vacationsForm.value);
+      const {name,lastName,email,phone,name_g,email_g,date_ini,date_fi,notes} = this.vacationsForm.value;
+      this.registerService.register(name,lastName,email,phone,name_g,email_g,date_ini,date_fi,notes).subscribe(
+        {
+          next: (response) => {
+            {console.log('Exitoso',response)};
+        },
+          error: error => {
+          console.error('Error en el registro del usuario', error);
+        },
+          complete:() => {
+          console.log("Envio de información completo!")
+         }
+        });
+
+    }else{
+      console.log('Formulario no valido');
     }
-    else{
-      console.log('Formulario invalido')
-    }
-  }
+  };
 }
